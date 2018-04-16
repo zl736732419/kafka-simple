3c941af15086bb0856df00d8c0443f2ec62487af
zhenglian <736732419@qq.com>
2018-04-16 16:44:58 +0800

    修改kafka消费者

--- src/main/java/com/zheng/kafka/SimpleConsumer.java
+++ src/main/java/com/zheng/kafka/SimpleConsumer.java
@@ -8,11 +8,47 @@ import java.util.Arrays;
 import java.util.Properties;
 
 /**
+ * kafkaconsumer是线程不安全的
  * @Author zhenglian
  * @Date 2018/1/30 21:28
  */
 public class SimpleConsumer {
     public static void main(String[] args) {
+//        autoOffsetCommit();
+        manualOffsetCommit();
+        
+    }
+
+    /**
+     * 手动控制consumer offset 
+     */
+    private static void manualOffsetCommit() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "zl50:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+                }
+                consumer.commitSync();
+            }
+        } finally {
+            consumer.close();
+        }
+        
+    }
+
+    /**
+     * 自动修改consumer offset
+     */
+    private static void autoOffsetCommit() {
         Properties props = new Properties();
         props.put("bootstrap.servers", "zl200:9092");
         props.put("group.id", "test");
@@ -22,11 +58,15 @@ public class SimpleConsumer {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("topic1"));
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records)
-                System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+        consumer.subscribe(Arrays.asList("test"));
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+            }
+        } finally {
+            consumer.close();
         }
     }
 }
